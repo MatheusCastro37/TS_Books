ee1de048222fe517aa6c730ee51c69adc8860ea9
Matheus Castro <mate.cas37@gmail.com>
2023-12-21 14:58:37 -0300

    feat: a simple server.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import fastify from "fastify";
+import "dotenv/config";
+
+const server = fastify();
+
+const port = process.env.PORT as number | undefined;
+
+server.get("/ping", async (request, reply) => {
+  return "pong?\n";
+});
+
+server.listen({ port: port }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
