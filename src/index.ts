d207d2296bb7b5ba839b837638d48b49da14956c
Matheus Castro <mate.cas37@gmail.com>
2023-12-26 13:33:13 -0300

    feat: add route create book

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
 import "dotenv/config";
 import { client } from "./database/mongo";
+import { CreateBookController } from "./controllers/create-book/create-book";
+import { CreateBookRepository } from "./repositories/create-book/create-book";
 
 const server = fastify();
 
@@ -8,9 +10,28 @@ const port = process.env.PORT as number | undefined;
 
 client.connect();
 
-server.get("/ping", async (request, reply) => {
-  return "pong?\n";
-});
+interface IBodyType {
+  name: string;
+  author: string;
+  publication_date: string;
+}
+
+server.post(
+  "/createBook",
+  async (request: FastifyRequest<{ Body: IBodyType }>, reply) => {
+    const mongoCreateRepository = new CreateBookRepository();
+
+    const createBookController = new CreateBookController(
+      mongoCreateRepository
+    );
+
+    const { statusCode, body } = await createBookController.handle(
+      request.body
+    );
+
+    reply.code(statusCode).send(body);
+  }
+);
 
 server.listen({ port: port }, (err, address) => {
   if (err) {
